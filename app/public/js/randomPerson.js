732a12742fe99058b8d3428110ec2ebadee06173
sheilalalala112 <xuanlanxu@hotmail.com>
2021-09-21 16:15:28 -0400

    organized script, css and randomuserinfo

--- app/public/js/randomPerson.js
+++ app/public/js/randomPerson.js
@@ -4,25 +4,33 @@ const RandomPerson = {
       return {
           // objects
         "person": {},
-
         }
     },
-    created() {
-        
-        //getting json data from 
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((parsedJson) => {
-           
-            this.person = parsedJson.results[0]
-           
-        })
-        .catch( err => {
-            console.error(err)
-        })
 
-        
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('DD/MM/YYYY');
+        }
+    },
+
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+            })
+            .catch( err => {
+                console.error(err)
+            })
+        }
+    },
+
+    created() {
+        this.fetchUserData();
     }
-  }
+}
   
 Vue.createApp(RandomPerson).mount('#personalinfo');
