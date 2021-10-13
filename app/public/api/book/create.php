06e0b251653de21c184564c113c52518e9497507
sheilalalala112 <xuanlanxu@hotmail.com>
2021-10-13 15:19:54 -0400

    problem in create.php

--- app/public/api/book/create.php
+++ app/public/api/book/create.php
@@ -54,4 +54,4 @@ $stmt->execute([
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other'); //303 means so far it's succeed, for the rest of the info go to offer/?student='
-header('Location: ../book');//what is this?
+header('Location: ../book');//what is this? maybe this is what causing the problem
