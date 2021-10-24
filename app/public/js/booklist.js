79c7a4e9a7e55b49bee7750f3a46bed9ebd918fb
sheilalalala112 <xuanlanxu@hotmail.com>
2021-10-24 18:47:38 -0400

    edit button for booklist

--- app/public/js/booklist.js
+++ app/public/js/booklist.js
@@ -2,7 +2,8 @@ const BookList = {
     data() {
       return {
         books: [],
-        bookForm: {}
+        bookForm: {},
+        selectedBook: null
       }
     },
 
@@ -21,10 +22,42 @@ const BookList = {
             })
         },
 
+        postBook(evt) { // what does this do?? 
+            console.log ("Test:", this.selectedBook);
+          if (this.selectedBook) {
+              this.postEditBook(evt);
+          } else {
+              this.postNewBook(evt);
+          }
+        },
+
+        postEditBook(evt) {
+            this.offerForm.id = this.selectedBook.id;
+            // this.offerForm.studentId = this.selectedStudent.id;        
+            
+            console.log("Editing!", this.bookForm);
+    
+            fetch('api/book/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+        },
+
         postNewBook(evt) { //event handler for form submission, event object is the default 
-            //select the student id and add another offer into this student     
-            console.log("Posting:", this.bookForm);
-            alert("Posting!");
+        console.log("Posting:", this.bookForm);
+        alert("Posting!");
 
             fetch('api/book/create.php', {
                 method:'POST',
@@ -42,6 +75,15 @@ const BookList = {
                 // reset the form
                 this.bookForm = {};
             });
+        },
+
+        handleEditBook(book) {
+            this.selectedBook = book;
+            this.bookForm = Object.assign({}, this.selectedBook);
+        },
+        handleResetEdit() { 
+            this.selectedBook = null;
+            this.bookForm = {};
         }
 
     },
